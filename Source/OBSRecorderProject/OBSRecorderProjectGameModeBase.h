a67195b095edda99c6baf4275d8097f6fc5713e5
Alper Gunes <43863818+TurkischeBaklava@users.noreply.github.com>
2023-07-09 19:31:57 +0100

    Initial project commit
    Created project at UE5.1.1 source build.
    Added few more gitignores

---
+++ Source/OBSRecorderProject/OBSRecorderProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OBSRecorderProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBSRECORDERPROJECT_API AOBSRecorderProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
