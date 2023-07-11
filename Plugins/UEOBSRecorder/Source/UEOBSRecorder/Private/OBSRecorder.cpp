55254c3487c89d6e662af0d36346fb4fa78bf989
Alper Gunes <43863818+TurkischeBaklava@users.noreply.github.com>
2023-07-12 00:20:48 +0100

    Updated
    Declared Log Categories
    Cleaned up GenerateAuthenticationKey function
    Aded RespondOpCode functions (can't use FString in switch-case)

--- Plugins/UEOBSRecorder/Source/UEOBSRecorder/Private/OBSRecorder.cpp
+++ Plugins/UEOBSRecorder/Source/UEOBSRecorder/Private/OBSRecorder.cpp
@@ -10,6 +10,10 @@
 #include "Containers/UnrealString.h"
 #include "WebSocketsModule.h"
 
+DEFINE_LOG_CATEGORY(LogWebSocket);
+DEFINE_LOG_CATEGORY(LogOBSRecorder);
+
+
 void UOBSRecorder::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
@@ -17,61 +21,44 @@ void UOBSRecorder::Initialize(FSubsystemCollectionBase& Collection)
 	if (!FModuleManager::Get().IsModuleLoaded("WebSockets"))
 	{
 		FModuleManager::Get().LoadModule("WebSockets");
-		UE_LOG(LogTemp, Warning, TEXT("WS module is loaded!"));
+		UE_LOG(LogWebSocket, Warning, TEXT("WS module is loaded!"));
 	}
 
-	const FString URL = TEXT("ws://localhost:4456");
+	const FString Host = TEXT("ws://localhost:");
+	const FString Port = TEXT("4456");
+	const FString URL = Host + Port;
 	const FString Protocol = TEXT("ws");
+	const FString Password = TEXT("yVlXQondHRJWsWuS");
 
 	WebSocket = FWebSocketsModule::Get().CreateWebSocket(URL, Protocol);
-	// FWebSocketsModule::Get().OnWebSocketCreated
-
-	WebSocket->OnConnected().AddLambda([]()
+	
+	WebSocket->OnConnected().AddLambda([Port,Protocol]()
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Connected to ws server"));
+		UE_LOG(LogWebSocket, Display, TEXT("Connected to websocket server succesfully: \n\tPort: %s\n\tProtocol: %s\n"),
+		       *Port, *Protocol);
 	});
 
-	WebSocket->OnConnectionError().AddLambda([](const FString& Error)
+	WebSocket->OnConnectionError().AddLambda([Port,Protocol](const FString& ErrorMessage)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Connection error: %s"), *Error);
+		UE_LOG(LogWebSocket, Error,
+		       TEXT("Failed to connect to WebSocket server: \n\tPort: %s\n\tProtocol: %s\n\tError Message: %s\n"),
+		       *Port, *Protocol, *ErrorMessage);
 	});
 
-	WebSocket->OnMessage().AddLambda([&](const FString Message)
+	WebSocket->OnMessage().AddLambda([&,Password](const FString Message)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Message received: %s"), *Message);
+		UE_LOG(LogOBSRecorder, Display, TEXT("Message received: %s"), *Message);
 
 		FJsonObjectWrapper JsonObjectWrapper;
 		UJsonBlueprintFunctionLibrary::FromString(this->GetWorld(), Message, JsonObjectWrapper);
 		const TSharedPtr<FJsonObject> OBSJsonResponse = JsonObjectWrapper.JsonObject;
 
 		const FString MessageType = OBSJsonResponse->GetStringField("op");
-		UE_LOG(LogTemp, Warning, TEXT("OpCode: %s"), *MessageType);
 		
-		//Generate key if OpCode 0 and verify
-		if ("0" == MessageType)
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Hello from OBSWebsocket"));
-
-			FString Challenge = OBSJsonResponse->GetObjectField("d")->GetObjectField("authentication")->GetStringField(
-				"challenge");
-			UE_LOG(LogTemp, Warning, TEXT("Challenge key:  %s"),*Challenge);
-
-			
-			FString Salt = OBSJsonResponse->GetObjectField("d")->GetObjectField("authentication")->
-			                                GetStringField("salt");
-			UE_LOG(LogTemp, Warning, TEXT("Salt key:  %s"),*Salt);
-
-			const FString AuthenticationKey = GenerateAuthenticationKey(Salt, Challenge);
-			
-			const FString VerifyMessage = FString::Printf(
-				TEXT("{\"op\": 1,\"d\": {\"rpcVersion\": 1,\"authentication\": \"%s\",\"eventSubscriptions\": 33}}"),*AuthenticationKey);
-			UE_LOG(LogTemp, Warning, TEXT("Verify Message: %s"),*VerifyMessage);
-			WebSocket->Send(VerifyMessage);
-		}
+		if(MessageType == "0") RespondOpCode0(OBSJsonResponse, Password);
+		else if (MessageType == "2") RespondOpCode2();
+	
 	});
-
-
-	WebSocket->Connect();
 }
 
 
@@ -85,43 +72,62 @@ void UOBSRecorder::Deinitialize()
 }
 
 
-FString UOBSRecorder::GenerateAuthenticationKey(const FString& Salt, const FString& Challenge)
+void UOBSRecorder::StartConnection()
 {
-	//Concatenate the websocket password with the salt provided by the server (password + salt)
-	FString Key = "yVlXQondHRJWsWuS" + Salt;
-
-	UE_LOG(LogTemp,Error,TEXT("Password + salt: %s"), *Key);
-	
-	//Generate an SHA256 binary hash of the result and base64 encode it, known as a base64 secret
-	//Concatenate the base64 secret with the challenge sent by the server (base64_secret + challenge)
-
-	FSHA256Hash Fsha256Hash;
-	
-	UHashSHA256BPLibrary::FromString(Fsha256Hash, Key);
-
-	uint8* Source = new uint8[32];
-	const uint32 Length = HexToBytes(Fsha256Hash.GetHash(),Source);							//From Hex string to byte array
-	UE_LOG(LogTemp,Error,TEXT("Base64(Key(SHA256)): %s"), *FBase64::Encode(Source,Length)); //Encode byte array
-
-	
-	Key = FBase64::Encode(Source,Length) + Challenge;
-	//UE_LOG(LogTemp,Error,TEXT("Base64(Key(SHA256)): %s"), *Base64.Encode(Fsha256Hash.GetHash()));
-	
-	UE_LOG(LogTemp,Error,TEXT("Base64(Key(SHA256))+Challenge = Key: %s"), *Key);
-	
-	
-	//Generate a binary SHA256 hash of that result and base64 encode it. You now have your authentication string.
-	FSHA256Hash Fsha256Hash2;
-	
-	UHashSHA256BPLibrary::FromString(Fsha256Hash2, Key);
-	
-	UE_LOG(LogTemp,Error,TEXT("Key(SHA2562): %s"), *Fsha256Hash2.GetHash());
-
-	HexToBytes(Fsha256Hash2.GetHash(),Source);
-	Key = FBase64::Encode(Source,Length);
-	UE_LOG(LogTemp,Error,TEXT("Base64(Key(SHA2562)): %s"), *Key);
-
-	delete[] Source;
-	return Key;
+	WebSocket->Connect();
 }
 
+FString UOBSRecorder::GenerateAuthenticationKey(const FString& Password, const FString& Salt, const FString& Challenge)
+{
+	//Concatenate the websocket password with the salt provided by the server (password + salt)
+	FString SecretString = Password + Salt;
+	//UE_LOG(LogTemp,Error,TEXT("Password + salt: %s"), *SecretString);
+
+	//Generate an SHA256 binary hash of the result and base64 encode it, known as a base64 secret
+	FSHA256Hash Fsha256Hash;
+	UHashSHA256BPLibrary::FromString(Fsha256Hash, SecretString);
+	FString SHA256 = Fsha256Hash.GetHash();
+	//UE_LOG(LogTemp,Error,TEXT("SHA256: %s"), *SHA256);
+
+	//Concatenate the base64 secret with the challenge sent by the server (base64_secret + challenge)
+	SecretString = HexToBase64(SHA256) + Challenge;
+
+	//Generate a binary SHA256 hash of that result and base64 encode it. You now have your authentication string.
+	UHashSHA256BPLibrary::FromString(Fsha256Hash, SecretString);
+	SHA256 = Fsha256Hash.GetHash();
+	SecretString = HexToBase64(SHA256);
+
+	return SecretString;
+}
+FString UOBSRecorder::HexToBase64(FString& HexString)
+{
+	uint8* Source = new uint8[32]; //TODO: Fix 
+	const uint32 Length = HexToBytes(HexString, Source); //Decode Hex String to byte arrays
+
+	HexString = FBase64::Encode(Source, Length); //Encode Base64
+
+	delete[] Source;
+	return HexString;
+}
+
+void UOBSRecorder::RespondOpCode0(const TSharedPtr<FJsonObject> OBSJsonResponse, const FString& Password)
+{
+	UE_LOG(LogOBSRecorder, Display, TEXT("Hello OBSWebsocket!"));
+	UE_LOG(LogOBSRecorder, Display, TEXT("Generating authenticator key and verifying client..."));
+
+	const FString Challenge = OBSJsonResponse->GetObjectField("d")->GetObjectField("authentication")->GetStringField(
+		"challenge");
+
+	const FString Salt = OBSJsonResponse->GetObjectField("d")->GetObjectField("authentication")->
+	                                      GetStringField("salt");
+
+	const FString AuthenticationKey = GenerateAuthenticationKey(Password, Salt, Challenge);
+
+	//Create Identify (OpCode 1) message
+	//TODO: Fix
+	const FString IdentifyMessage = FString::Printf(
+		TEXT("{\"op\": 1,\"d\": {\"rpcVersion\": 1,\"authentication\": \"%s\",\"eventSubscriptions\": 33}}"),
+		*AuthenticationKey);
+
+	WebSocket->Send(IdentifyMessage); //Sends 
+}
--- Plugins/UEOBSRecorder/Source/UEOBSRecorder/Public/OBSRecorder.h
+++ Plugins/UEOBSRecorder/Source/UEOBSRecorder/Public/OBSRecorder.h
@@ -7,6 +7,9 @@
 #include "IWebSocket.h"
 #include "OBSRecorder.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogOBSRecorder, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogWebSocket, Log, All);
+
 /**
  * 
  */
@@ -22,8 +25,17 @@ public:
 	
 	TSharedPtr<IWebSocket> WebSocket;
 
+	UFUNCTION(BlueprintCallable)
+	void StartConnection();
+	
 	//Generates the key
-	static FString GenerateAuthenticationKey(const FString& Salt,const FString& Challenge);
+	static FString GenerateAuthenticationKey(const FString& Password,const FString& Salt,const FString& Challenge);
+	
+	static FString HexToBase64(FString& HexString);
 	
 
+private:
+
+	void RespondOpCode0(const TSharedPtr<FJsonObject> OBSJsonResponse,const FString& Password);
+	void RespondOpCode2(){UE_LOG(LogOBSRecorder,Log,TEXT("The identify request was received and validated, and the connection is now ready for normal operation."))}
 };
